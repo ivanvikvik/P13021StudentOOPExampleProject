b91379827bb39f1cc4997be523e0c984424e1aed
Masha <mashas@li.ru>
2022-04-06 17:37:37 +0300

    Add Group class and its interface

---
+++ P13021StudentOOPExampleProject/Group.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Header.h"
+#include "Student.h"
+
+class Group {
+private:
+	string name;
+	int count;
+	Student* list;
+
+public:
+	Group();
+	Group(string name);
+	Group(string name, Student* list, int count);
+	~Group();
+
+	string getName();
+	void setName(string name);
+
+	int getCount();
+	Student get(int index);
+
+	string getInfo();
+};
+
